eabfbd7be10c50dc2459b9c770f62a2315c5e4a1
suraj <jsuraj61@gmail.com>
2023-03-11 23:16:58 +0530

    Database schema with dummy data

--- Database schema/0 overall-database.sql
+++ Database schema/0 overall-database.sql
@@ -4,6 +4,6 @@ CREATE DATABASE tastytreat;
 -- create user root to handle db txns on tastytreat db
 CREATE USER 'root'@'localhost' IDENTIFIED BY 'tastytreat';
 ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'tastytreat';
-GRANT ALL PRIVILEGES ON hbdemo.* to hbadmin@localhost;
+GRANT ALL PRIVILEGES ON tastytreat.* to root@localhost;
 
 -- mysql -uhbadmin -ptastytreat tastytreat
